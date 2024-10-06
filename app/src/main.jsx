b59f252075dc465a6cde8067dd7782f1e90b94f7
Brock <brockrs@live.com>
2024-10-06 10:22:01 -0400

    Added more pages to better reflect the navbar

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -11,12 +11,18 @@ import '@fontsource/inter';
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Attorneys from "./pages/Attorneys.jsx";
+import FAQs from "./pages/FAQs.jsx";  
+import Resources from "./pages/Resources.jsx";
 import { I18nextProvider } from "react-i18next";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/About", element: <About /> },
   { path: "/contact", element: <Contact /> },
+  {path: "/Attorneys", element: <Attorneys />},
+  {path: "FAQs", element: <FAQs />},
+  {path: "/Resources", element: <Resources />},
 ]);
 
 createRoot(document.getElementById("root")).render(

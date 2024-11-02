43787db7016b5960300dac776dccc29e0b6ce896
ethans333 <gamesniper99@live.com>
2024-11-02 10:52:50 -0400

    fixed get started button

--- app/src/components/GetStartedBanner.jsx
+++ app/src/components/GetStartedBanner.jsx
@@ -1,6 +1,9 @@
 import Button from "@mui/joy/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function GetStartedBanner() {
+  const navigate = useNavigate();
+
   return (
     <div className="flex w-full bg-crimson text-white py-24">
       <div className="max-w-7xl w-full mx-auto space-y-10">
@@ -15,7 +18,11 @@ export default function GetStartedBanner() {
           Commodo tempor reprehenderit exercitation qui velit dolore tempor sit.
         </p>
         <div className="flex justify-center">
-          <Button color="neutral" variant="soft">
+          <Button
+            color="neutral"
+            variant="soft"
+            onClick={() => navigate("/contact")}
+          >
             Get Started
           </Button>
         </div>
